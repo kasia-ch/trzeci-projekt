fef751f1dcbbbd651285dbd35f8f58eb986331da
My Love <checinska.k@gmail.com>
2019-10-09 17:36:42 +0200

    in catch validation of the error added ; imBusy : false

--- src/App.js
+++ src/App.js
@@ -77,6 +77,7 @@ class App extends React.Component {
         console.log('error', this.state, error);
         this.setState(prevState => ({
           error: true,
+          imBusy: false,
         }));
       })
   }
